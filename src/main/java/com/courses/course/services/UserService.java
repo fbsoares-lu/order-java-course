a6d8e9689c2993abc49484e268c314d452f5c4f9
fbsoares-lu <lsoareshfb@gmail.com>
2021-06-02 20:38:16 -0300

    #Feature: create new validations

--- src/main/java/com/courses/course/services/UserService.java
+++ src/main/java/com/courses/course/services/UserService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.courses.course.entities.User;
 import com.courses.course.repositories.UserRepository;
+import com.courses.course.services.exceptions.DatabaseException;
+import com.courses.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,8 +24,8 @@ public class UserService {
 	}
 	
 	public User findById(Long id) {
-		Optional<User> user = repository.findById(id);
-		return user.get();
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
@@ -29,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
---
+++ src/main/java/com/courses/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.courses.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
